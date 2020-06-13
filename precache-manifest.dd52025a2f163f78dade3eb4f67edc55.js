eb48387633affdd49511d56c0438fcc397246c8b
sidhant <sidhantsharma293@gmail.com>
2020-06-13 19:05:55 +0530

    Updates

---
+++ precache-manifest.dd52025a2f163f78dade3eb4f67edc55.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91cdda85985eca8ef90fd230c4642e6a",
+    "url": "/Corona-Tracker/index.html"
+  },
+  {
+    "revision": "d156c9cf24b32869f12a",
+    "url": "/Corona-Tracker/static/css/main.5b83b041.chunk.css"
+  },
+  {
+    "revision": "1e672d6911af06f050ca",
+    "url": "/Corona-Tracker/static/js/2.e0762477.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Corona-Tracker/static/js/2.e0762477.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d156c9cf24b32869f12a",
+    "url": "/Corona-Tracker/static/js/main.e5554269.chunk.js"
+  },
+  {
+    "revision": "995dcb91a1d109b30e84",
+    "url": "/Corona-Tracker/static/js/runtime-main.334d4e64.js"
+  }
+]);
